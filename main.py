9bc3e510f4b262d86be272614f9a86c5bb37b27f
evitaieva <868717711a@gmail.com>
2024-09-24 19:25:30 +0300

    number 5

--- main.py
+++ main.py
@@ -18,15 +18,23 @@
 # if age >= 18:
 #     print(name + ' Jus galite balsuoti')
 
-print('prasau parasykite 4 skaicius')
-skai1 = int(input())
-skai2 = int(input())
-skai3 = int(input())
-skai4 = int(input())
-vidurkis = (skai1 + skai2 + skai3 + skai4) / 4
-print(vidurkis)
-if vidurkis >=5:
-    print('vidurkis teigiamas')
-if vidurkis <5:
-    print('vidurkis neigiamas')
+# print('prasau parasykite 4 skaicius')
+# skai1 = int(input())
+# skai2 = int(input())
+# skai3 = int(input())
+# skai4 = int(input())
+# vidurkis = (skai1 + skai2 + skai3 + skai4) / 4
+# print(vidurkis)
+# if vidurkis >=5:
+#     print('vidurkis teigiamas')
+# if vidurkis <5:
+#     print('vidurkis neigiamas')
+
+print('prasau parasykite skaiciu')
+number = int(input())
+if number % 5 ==0:
+    print(number * 1, '\n', number * 2, '\n', number * 3, '\n', number * 4, '\n', number * 5 )
+else:
+    print('skaicius nesidalina is 5')
+
 
