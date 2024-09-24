3b0c0f638ba065d66e6d73beb8ce35dfc3d532f9
evitaieva <868717711a@gmail.com>
2024-09-24 21:31:23 +0300

    maziausias skaicius,

--- main.py
+++ main.py
@@ -94,13 +94,24 @@ import pathlib
 # else:
 #     print("message of dissapointment")
 
+# print('prasau parasykite 3 skaicius')
+# number1 = int(input())
+# number2 = int(input())
+# number3 = int(input())
+# if (number1 > number2) and (number1 > number3):
+#     print('number1 yra didziausias')
+# elif (number2 > number1) and (number2 > number3):
+#     print('number2 yra yra didziausias')
+# elif (number3 > number1) and (number3 > number2):
+#     print('number3 yra yra didziausias')
+
 print('prasau parasykite 3 skaicius')
 number1 = int(input())
 number2 = int(input())
 number3 = int(input())
-if (number1 > number2) and (number1 > number3):
-    print('number1 yra didziausias')
-elif (number2 > number1) and (number2 > number3):
-    print('number2 yra yra didziausias')
-elif (number3 > number1) and (number3 > number2):
-    print('number3 yra yra didziausias')
+if (number1 < number2) and (number1 < number3):
+    print('number1 yra maziausias')
+elif (number2 < number1) and (number2 < number3):
+    print('number2 yra yra maziausias')
+elif (number3 < number1) and (number3 < number2):
+    print('number3 yra yra maziausias')
