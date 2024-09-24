30d9ac7e0b2292159bd76b716f733a2be6777ee8
evitaieva <868717711a@gmail.com>
2024-09-24 18:53:47 +0300

    hello hello 9/24 voting age

--- main.py
+++ main.py
@@ -5,5 +5,16 @@
 #
 # print('pradedu rink zaliu staskiukus')
 
-print('hello')
-print("hello 9/24")
+# print('hello')
+# print("hello 9/24")
+
+print('prasau parasykite savo varda')
+name = input()
+print('koks Jusu amzius')
+age = int(input())
+if age < 18:
+    print(name + ', jus per jaunas')
+if age >= 18:
+    print(name + ' Jus galite balsuoti')
+
+
