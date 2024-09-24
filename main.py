69bf8da2f4038f3d72dacdb0f4d4f92b9c59f0ee
evitaieva <868717711a@gmail.com>
2024-09-24 19:06:54 +0300

    vidurkis

--- main.py
+++ main.py
@@ -8,13 +8,25 @@
 # print('hello')
 # print("hello 9/24")
 
-print('prasau parasykite savo varda')
-name = input()
-print('koks Jusu amzius')
-age = int(input())
-if age < 18:
-    print(name + ', jus per jaunas')
-if age >= 18:
-    print(name + ' Jus galite balsuoti')
+#
+# print('prasau parasykite savo varda')
+# name = input()
+# print('koks Jusu amzius')
+# age = int(input())
+# if age < 18:
+#     print(name + ', jus per jaunas')
+# if age >= 18:
+#     print(name + ' Jus galite balsuoti')
 
+print('prasau parasykite 4 skaicius')
+skai1 = int(input())
+skai2 = int(input())
+skai3 = int(input())
+skai4 = int(input())
+vidurkis = (skai1 + skai2 + skai3 + skai4) / 4
+print(vidurkis)
+if vidurkis >=5:
+    print('vidurkis teigiamas')
+if vidurkis <5:
+    print('vidurkis neigiamas')
 
