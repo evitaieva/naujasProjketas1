e7bdd1ce859e14ec1a3df505f20ec892a9ad3fe4
evitaieva <868717711a@gmail.com>
2024-09-24 19:51:27 +0300

    if true/falls

--- main.py
+++ main.py
@@ -32,9 +32,29 @@
 
 print('prasau parasykite skaiciu')
 number = int(input())
+isItGood = False
 if number % 5 ==0:
     print(number * 1, '\n', number * 2, '\n', number * 3, '\n', number * 4, '\n', number * 5 )
+    isItGood = True
+if number % 2 ==0:
+    print(number,'\n', number * number, '\n', number / 2)
+    isItGood = True
+if number % 7 != 0:
+    print('prasau parasykite kita skaiciu')
+    number2 = int(input())
+    print( number2 + number2, '\n', number2 - number2, '\n', number2 * number2, '\n', number2 / number2)
+    isItGood = True
+if not isItGood:
+    print("message of dissapointment")
+
+
+if number % 5 ==0:
+    print(number * 1, '\n', number * 2, '\n', number * 3, '\n', number * 4, '\n', number * 5 )
+elif number % 2 ==0:
+    print(number,'\n', number * number, '\n', number / 2)
+elif number % 7 != 0:
+    print('prasau parasykite kita skaiciu')
+    number2 = int(input())
+    print( number2 + number2, '\n', number2 - number2, '\n', number2 * number2, '\n', number2 / number2)
 else:
-    print('skaicius nesidalina is 5')
-
-
+    print("message of dissapointment")
