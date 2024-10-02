d3b5a96d6484072c7dd0552b4ef9a055a7978bcf
evitaieva <868717711a@gmail.com>
2024-10-03 01:55:37 +0300

    for19 kodel ne and o or?

--- main.py
+++ main.py
@@ -797,13 +797,22 @@ from traceback import print_list
 # print “Buzz”. For numbers which are multiples of both three and five print
 # “FizzBuzz”."
 
+# FizzBuzz = 0
+# for i in range(1, 100):
+#    if i % 3 ==0:
+#       print('Fizz')
+#       if i % 5 ==0:
+#          print('Buzz')
+#             if i % 3 ==0 and 5 ==0:
+#                print('FizzBuzz')
+
+
 FizzBuzz = 0
 for i in range(1, 100):
-   if i % 3 ==0:
-      print('Fizz')
-      if i % 5 ==0:
-         print('Buzz')
-            if i % 3 ==0 and 5 ==0:
-               print('FizzBuzz')
-
+   if i % 3 == 0 or 5 == 0:
+      print('FizzBuzz')
+      if i % 3 ==0:
+         print('Fizz')
+         if i % 5 ==0:
+            print('Buzz')
 
