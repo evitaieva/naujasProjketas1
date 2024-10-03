e2fed768ba9e5b6629f34ae22e469bc988a29640
evitaieva <868717711a@gmail.com>
2024-10-03 19:12:57 +0300

    10.4 done

--- main.py
+++ main.py
@@ -807,16 +807,16 @@ from traceback import print_list
 #                print('FizzBuzz')
 
 
-FizzBuzz = 0
-for i in range(1, 100):
-   if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz')
-   elif i % 3 == 0:
-        print('Fizz')
-   elif i % 5 == 0:
-        print('Buzz')
-   else:
-       print(i)
+# FizzBuzz = 0
+# for i in range(1, 100):
+#    if i % 3 == 0 and i % 5 == 0:
+#         print('FizzBuzz')
+#    elif i % 3 == 0:
+#         print('Fizz')
+#    elif i % 5 == 0:
+#         print('Buzz')
+#    else:
+#        print(i)
 
 # Išveskite visus skaičius nuo 1 iki 20.
 #
@@ -858,9 +858,9 @@ for i in range(1, 100):
 # Išveskite visus skaičius nuo 1 iki 100
 # arba iki tol kol pasitaikys toks, kuris dalinasi iš 7.
 
-# number = 0
-# while number <= 100:
-#     number += 1
-#     print(number)
-#     if number % 7 == 0:
-#        break
+number = 0
+while number <= 100:
+    number += 1
+    print(number)
+    if number % 7 == 0:
+       break
